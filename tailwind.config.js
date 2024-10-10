2688597ec93229e9537f89ea69f6cc58ca26413c
JV1T0R <jvesteves45@gmail.com>
2024-10-10 16:37:03 -0300

    Styles adjsutments

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        spacegrotesk: ['"Space Grotesk"', 'sans-serif'],
+        jersey: ['"Jersey 10"', 'sans-serif'],
         jetmono: ['"JetBrains Mono"', 'monospace'], 
       },
       colors: {
