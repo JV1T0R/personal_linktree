651ec8453fa614ec795c270bb3b64a5beb51c752
João Vítor Pamponet Esteves <jvticontato@gmail.com>
2025-06-11 17:21:13 -0300

    first commit

---
+++ public/main.js
@@ -0,0 +1,13 @@
+// Function to redirect to the links
+document.addEventListener('DOMContentLoaded', function() {
+    const linkButtons = document.querySelectorAll('.link-btn')
+    
+    linkButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const url = this.getAttribute('data-url')
+            if (url) {
+                window.open(url);
+            }
+        })
+    })
+})
